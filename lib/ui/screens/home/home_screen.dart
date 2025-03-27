faf6c93e1f44767a0d122a54067500b157b46836
Aitly213 <aitalyasanbaev@gmail.com>
2025-03-27 15:29:52 +0600

    static google maps

--- lib/ui/screens/home/home_screen.dart
+++ lib/ui/screens/home/home_screen.dart
@@ -1,19 +1,82 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Location _locationController = new Location();
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(42.875639, 74.603806),
+    zoom: 13,
+  );
+  static const LatLng _pLoca = LatLng(42.87926, 74.61614);
+  LatLng? _currentP = null;
+
+  @override
+  void initState() {
+    super.initState();
+
+    getLocationUpdates();
+  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-         body: Column(
-           children: [
-             Text("HomeScreen")
-           ],
-         ),
+        body: GoogleMap(
+          initialCameraPosition: _kGooglePlex,
+          markers: {
+            Marker(
+                markerId: MarkerId("_currentLocation"),
+                icon: BitmapDescriptor.defaultMarkerWithHue(
+                    BitmapDescriptor.hueBlue),
+                position: _pLoca)
+          },
+        ),
       ),
     );
+  }
+
+  Future<void> getLocationUpdates() async {
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+
+    _serviceEnabled = await _locationController.serviceEnabled();
+    if (_serviceEnabled) {
+      _serviceEnabled = await _locationController.requestService();
+    } else {
+      return;
+    }
+
+    _permissionGranted = await _locationController.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await _locationController.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    _locationController.onLocationChanged
+        .listen((LocationData currentLocation) {
+      if (currentLocation.latitude != null &&
+          currentLocation.longitude != null) {
+        setState(() {
+          _currentP =
+              LatLng(currentLocation.latitude!, currentLocation.longitude!);
+
+          print(_currentP);
+          print(
+              "-----------------------------------------------------------------------------"
+              "-----------------------------------------------------------------------------");
+        });
+      }
+    });
   }
 }
