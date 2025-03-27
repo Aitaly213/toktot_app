d019dc8b95960cc569337bd8410d72376c313e6f
Aitly213 <aitalyasanbaev@gmail.com>
2025-03-27 20:10:19 +0600

    started to do directions

--- lib/ui/screens/home/home_screen.dart
+++ lib/ui/screens/home/home_screen.dart
@@ -1,6 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
+
+const String GOOGLE_API_KEY = "AIzaSyByuplGDm9Wfby4EFaz25BbTNbgrfDyRjg";
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -13,10 +18,16 @@ class _HomeScreenState extends State<HomeScreen> {
   Location _locationController = new Location();
   Set<Marker> _markers = {};
 
+  final Completer<GoogleMapController> _mapController =
+  Completer<GoogleMapController>();
+
   static const _kGooglePlex = CameraPosition(
     target: LatLng(42.875639, 74.603806),
     zoom: 13,
   );
+
+  static const _posDestination = LatLng(42.828061, 74.601591);
+  static const _posOrigin = LatLng(42.808061, 74.621591);
 
   LatLng? _currentP = null;
 
@@ -31,10 +42,35 @@ class _HomeScreenState extends State<HomeScreen> {
     return SafeArea(
       child: Scaffold(
         body: GoogleMap(
+          onMapCreated: ((GoogleMapController controller) =>
+              _mapController.complete(controller)),
           initialCameraPosition: _kGooglePlex,
-          markers: _markers,
+          markers: {
+            Marker(
+              markerId: MarkerId("_currentLocation"),
+              icon: BitmapDescriptor.defaultMarkerWithHue(
+                  BitmapDescriptor.hueBlue),
+              position: _currentP!, //NULL AT THE BEGining
+              infoWindow: InfoWindow(title: "Ваше местоположение"),
+            ),
+            Marker(
+              markerId: MarkerId("_destinationLocation"),
+              icon: BitmapDescriptor.defaultMarkerWithHue(
+                  BitmapDescriptor.hueBlue),
+              position: _posDestination,
+            ),
+          },
         ),
       ),
+    );
+  }
+
+  Future<void> _cameraToPosition(LatLng position) async {
+    final GoogleMapController controller = await _mapController.future;
+    CameraPosition _newCameraPosition =
+    CameraPosition(target: position, zoom: 13);
+    await controller.animateCamera(
+      CameraUpdate.newCameraPosition(_newCameraPosition),
     );
   }
 
@@ -58,24 +94,40 @@ class _HomeScreenState extends State<HomeScreen> {
       }
     }
 
-    _locationController.onLocationChanged.listen((LocationData currentLocation) {
-      if (currentLocation.latitude != null && currentLocation.longitude != null) {
+    _locationController.onLocationChanged
+        .listen((LocationData currentLocation) {
+      if (currentLocation.latitude != null &&
+          currentLocation.longitude != null) {
         setState(() {
-          _currentP = LatLng(currentLocation.latitude!, currentLocation.longitude!);
-
-          // Обновляем маркеры
-          _markers = {
-            Marker(
-              markerId: MarkerId("_currentLocation"),
-              icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
-              position: _currentP!,
-              infoWindow: InfoWindow(title: "Ваше местоположение"),
-            )
-          };
-
-          print(_currentP);
+          _currentP =
+              LatLng(currentLocation.latitude!, currentLocation.longitude!);
+          _cameraToPosition(_currentP!);
         });
       }
     });
+  }
+
+  Future<List<LatLng>> getPolylinePoints() async {
+    List<LatLng> polylineCoordinates = [];
+    PolylinePoints polylinePoints = PolylinePoints();
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+      googleApiKey: GOOGLE_API_KEY,
+      request: PolylineRequest(
+        origin: PointLatLng(_posOrigin.latitude, _posOrigin.longitude),
+        destination: PointLatLng(
+            _posDestination.latitude, _posDestination.longitude),
+        mode: TravelMode.driving,
+        wayPoints: [PolylineWayPoint(location: "Sabo, Yaba Lagos Nigeria")],
+      ),
+    );
+    if(result.points.isNotEmpty){
+      result.points.forEach((PointLatLng point){
+        polylineCoordinates.add(LatLng(point.latitude, point.longitude));
+      });
+    }else{
+      print("ERRORRRRR---------------------");
+    }
+
+    return polylineCoordinates;
   }
 }
